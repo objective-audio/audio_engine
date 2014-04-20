970d87d24abc6b9fdb64bed0b714be228ad4a7e7
Yuki Yasoshima <yukiyasos@gmail.com>
2014-04-20 13:46:11 +0900

    YASAudioConnectionのコメントを修正

--- YASAudio/YASAudioConnection.h
+++ YASAudio/YASAudioConnection.h
@@ -15,9 +15,9 @@
 
 @interface YASAudioConnection : NSObject
 
-@property (nonatomic, strong) YASAudioNode *sourceNode; /*!< 接続元のノードです */
-@property (nonatomic, assign) UInt32 sourceOutputNumber; /*!< 接続元のバス番号です */
-@property (nonatomic, strong) YASAudioNode *destNode; /*!< 接続先のノードです */
-@property (nonatomic, assign) UInt32 destInputNumber; /*!< 接続先のバス番号です */
+@property (nonatomic, strong) YASAudioNode *sourceNode; //!< 接続元のノードです
+@property (nonatomic, assign) UInt32 sourceOutputNumber; //!< 接続元のバス番号です
+@property (nonatomic, strong) YASAudioNode *destNode; //!< 接続先のノードです
+@property (nonatomic, assign) UInt32 destInputNumber; //!< 接続先のバス番号です
 
 @end
