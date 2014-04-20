f608327486572ccd26b72d3396b1e23c31d63c6a
Yuki Yasoshima <yukiyasos@gmail.com>
2014-04-20 15:58:18 +0900

    YASAudioUtilitiesのコメントを修正

--- YASAudio/YASAudioUtilities.h
+++ YASAudio/YASAudioUtilities.h
@@ -7,36 +7,36 @@
 #import <Foundation/Foundation.h>
 #import <AudioToolbox/AudioToolbox.h>
 
-/*! AudioBufferListを生成します */
+//! AudioBufferListを生成します
 AudioBufferList *YASAllocateAudioBufferList(UInt32 bufferCount, UInt32 ch, UInt32 size);
 
-/*! AudioBufferListを解放します */
+//! AudioBufferListを解放します
 void YASRemoveAudioBufferList(AudioBufferList *list);
 
-/*! AudioBufferListのバッファをゼロでクリアします */
+//! AudioBufferListのバッファをゼロでクリアします
 void YASClearAudioBufferList(AudioBufferList *list);
 
-/*! AudioBufferListにサイン波を書き込みます（デバッグ用） */
+//! AudioBufferListにサイン波を書き込みます（デバッグ用）
 void YASFillFloat32SinewaveToAudioBufferList(AudioBufferList *list, UInt32 cycle);
 
-/*! リニア値をdB値に変換します */
+//! リニア値をdB値に変換します
 CGFloat YASDBValueFromLinearValue(CGFloat val);
 
-/*! dB値をリニア値に変換します */
+//! dB値をリニア値に変換します
 CGFloat YASLinearValueFromDBValue(CGFloat val);
 
-/*! 秒数をテンポに変換します */
+//! 秒数をテンポに変換します
 CGFloat YASTempoValueFromSeconds(CGFloat sec);
 
-/*! テンポを秒数に変換します */
+//! テンポを秒数に変換します
 CGFloat YASSecondsFromTempoValue(CGFloat tempo);
 
-/*! ASBDにFloat32・NonInterleavedのデータをセットします */
+//! ASBDにFloat32・NonInterleavedのデータをセットします
 void YASGetFloat32NonInterleavedStereoFormat(AudioStreamBasicDescription *outFormat, Float64 sampleRate);
 
-/*! ASBDにSInt16・Interleavedのデータをセットします */
+//! ASBDにSInt16・Interleavedのデータをセットします
 void YASGetSInt16InterleavedStereoFormat(AudioStreamBasicDescription *outFormat, Float64 sampleRate);
 
-/*! フレーム数を秒数に変換します */
+//! フレーム数を秒数に変換します
 NSTimeInterval YASSecFromFrames(UInt32 frames, Float64 sampleRate);
 
