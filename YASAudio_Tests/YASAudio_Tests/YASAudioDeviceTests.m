6f6f3650373e9e19e83509f6eaf485dac004aa60
Yuki Yasoshima <yukiyasos@gmail.com>
2015-02-14 19:57:37 +0900

    Add AudioDevice test file

---
+++ YASAudio_Tests/YASAudio_Tests/YASAudioDeviceTests.m
@@ -0,0 +1,25 @@
+//
+//  YASAudioDeviceTests.m
+//  Copyright (c) 2015 Yuki Yasoshima.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface YASAudioDeviceTests : XCTestCase
+
+@end
+
+@implementation YASAudioDeviceTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+@end
