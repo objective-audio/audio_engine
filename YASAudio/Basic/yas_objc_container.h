a182f340f31800a6f086a8caf9869c2227cd32b7
Yuki Yasoshima <yukiyasos@gmail.com>
2015-07-16 11:10:15 +0900

    refine objc container

--- YASAudio/Basic/yas_objc_container.h
+++ YASAudio/Basic/yas_objc_container.h
@@ -8,28 +8,48 @@
 #include <Foundation/Foundation.h>
 #include <mutex>
 #include <memory>
+#include "YASMacros.h"
 
 namespace yas
 {
+    // clang-format off
+    
     class objc_container;
     using objc_container_ptr = std::shared_ptr<objc_container>;
+
+    struct strong_t {};
+    struct weak_t {};
+    constexpr strong_t strong = strong_t();
+    constexpr weak_t weak = weak_t();
+
+    // clang-format on
 
     class objc_container
     {
        public:
         static objc_container_ptr create();
         static objc_container_ptr create(const id object);
+        static objc_container_ptr create(const id object, strong_t);
+        static objc_container_ptr create(const id object, weak_t);
 
         objc_container();
-        explicit objc_container(const id object);
+        objc_container(const id object, strong_t);
+        objc_container(const id object, weak_t);
         ~objc_container();
+
+        objc_container(const objc_container &);
+        objc_container(objc_container &&);
+        objc_container &operator=(const objc_container &);
+        objc_container &operator=(objc_container &&);
 
         void set_object(const id object);
         id retained_object() const;
         id autoreleased_object() const;
 
        private:
-        __unsafe_unretained id _objc_object;
+        id _strong_object;
+        YASWeakForVariable id _weak_object;
         mutable std::recursive_mutex _mutex;
+        bool _is_strong;
     };
 }
--- YASAudio_Tests/YASAudio_Tests/yas_objc_container_tests.mm
+++ YASAudio_Tests/YASAudio_Tests/yas_objc_container_tests.mm
@@ -40,6 +40,8 @@ static int _objc_object_count = 0;
 - (void)setUp
 {
     [super setUp];
+
+    _objc_object_count = 0;
 }
 
 - (void)tearDown
@@ -47,16 +49,14 @@ static int _objc_object_count = 0;
     [super tearDown];
 }
 
-- (void)testUnretainedObjcObject
+- (void)testUnretainObjcObjectOnWeakContainer
 {
-    _objc_object_count = 0;
-
     YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
 
     XCTAssertEqual(_objc_object_count, 1);
     XCTAssertEqual([objc_object retainCount], 1);
 
-    if (auto objc_container = yas::objc_container::create(objc_object)) {
+    if (auto objc_container = yas::objc_container::create(objc_object, yas::weak)) {
         XCTAssertEqual(_objc_object_count, 1);
         XCTAssertEqual([objc_object retainCount], 1);
     }
@@ -67,10 +67,26 @@ static int _objc_object_count = 0;
     XCTAssertEqual(_objc_object_count, 0);
 }
 
-- (void)testSetObjcObject
+- (void)testRetainObjcObjectOnStrongContainer
 {
-    _objc_object_count = 0;
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
 
+    XCTAssertEqual(_objc_object_count, 1);
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    if (auto objc_container = yas::objc_container::create(objc_object, yas::strong)) {
+        XCTAssertEqual(_objc_object_count, 1);
+        XCTAssertEqual([objc_object retainCount], 2);
+    }
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+- (void)testSetObjcObjectOnWeakContainer
+{
     YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
 
     if (auto objc_container = yas::objc_container::create()) {
@@ -111,6 +127,299 @@ static int _objc_object_count = 0;
 
     [objc_object release];
     objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+- (void)testSetObjcObjectOnStrongContainer
+{
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
+
+    if (auto objc_container = yas::objc_container::create(nil, yas::strong)) {
+        XCTAssertNil(objc_container->retained_object());
+        XCTAssertNil(objc_container->autoreleased_object());
+        XCTAssertEqual(_objc_object_count, 1);
+        XCTAssertEqual([objc_object retainCount], 1);
+
+        objc_container->set_object(objc_object);
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        id retained_object = objc_container->retained_object();
+
+        XCTAssertNotNil(retained_object);
+        XCTAssertEqual([objc_object retainCount], 3);
+
+        [objc_object release];
+        retained_object = nil;
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        @autoreleasepool
+        {
+            id autoreleased_object = objc_container->autoreleased_object();
+
+            XCTAssertNotNil(autoreleased_object);
+            XCTAssertEqual([objc_object retainCount], 3);
+        }
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        objc_container->set_object(nil);
+
+        XCTAssertNil(objc_container->retained_object());
+        XCTAssertNil(objc_container->autoreleased_object());
+        XCTAssertEqual(_objc_object_count, 1);
+        XCTAssertEqual([objc_object retainCount], 1);
+    }
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+#pragma mark - copy
+
+- (void)testCopyStrongToStrong
+{
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    {
+        yas::objc_container objc_container1(objc_object, yas::strong);
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        yas::objc_container objc_container2(objc_object, yas::strong);
+
+        XCTAssertEqual([objc_object retainCount], 3);
+
+        objc_container1 = objc_container2;
+
+        XCTAssertEqual([objc_object retainCount], 3);
+    }
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+- (void)testCopyWeakToStrong
+{
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    {
+        yas::objc_container objc_container1(objc_object, yas::strong);
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        yas::objc_container objc_container2(objc_object, yas::weak);
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        objc_container1 = objc_container2;
+
+        XCTAssertEqual([objc_object retainCount], 1);
+    }
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+- (void)testCopyStrongToWeak
+{
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    {
+        yas::objc_container objc_container1(objc_object, yas::weak);
+
+        XCTAssertEqual([objc_object retainCount], 1);
+
+        yas::objc_container objc_container2(objc_object, yas::strong);
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        objc_container1 = objc_container2;
+
+        XCTAssertEqual([objc_object retainCount], 3);
+    }
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+- (void)testCopyWeakToWeak
+{
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    {
+        yas::objc_container objc_container1(objc_object, yas::weak);
+
+        XCTAssertEqual([objc_object retainCount], 1);
+
+        yas::objc_container objc_container2(objc_object, yas::weak);
+
+        XCTAssertEqual([objc_object retainCount], 1);
+
+        objc_container1 = objc_container2;
+
+        XCTAssertEqual([objc_object retainCount], 1);
+    }
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+- (void)testCopyConstructorStrong
+{
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    {
+        yas::objc_container objc_container1(objc_object, yas::strong);
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        yas::objc_container objc_container2(objc_container1);
+
+        XCTAssertEqual([objc_object retainCount], 3);
+    }
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+- (void)testCopyConstructorWeak
+{
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    {
+        yas::objc_container objc_container1(objc_object, yas::weak);
+
+        XCTAssertEqual([objc_object retainCount], 1);
+
+        yas::objc_container objc_container2(objc_container1);
+
+        XCTAssertEqual([objc_object retainCount], 1);
+    }
+
+    XCTAssertEqual([objc_object retainCount], 1);
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+- (void)testCopyDifferentObjcObjects
+{
+    YASObjCContainerTest *objc_object1 = [[YASObjCContainerTest alloc] init];
+    YASObjCContainerTest *objc_object2 = [[YASObjCContainerTest alloc] init];
+
+    {
+        yas::objc_container objc_container1(objc_object1, yas::strong);
+        yas::objc_container objc_container2(objc_object2, yas::strong);
+
+        XCTAssertEqual([objc_object1 retainCount], 2);
+        XCTAssertEqual([objc_object2 retainCount], 2);
+
+        objc_container1 = objc_container2;
+
+        XCTAssertEqual([objc_object1 retainCount], 1);
+        XCTAssertEqual([objc_object2 retainCount], 3);
+    }
+
+    [objc_object1 release];
+    objc_object1 = nil;
+    [objc_object2 release];
+    objc_object2 = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+#pragma mark - move
+
+- (void)testMove
+{
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
+
+    {
+        yas::objc_container objc_container1(objc_object, yas::strong);
+        yas::objc_container objc_container2;
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        objc_container2 = std::move(objc_container1);
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        XCTAssertNil(objc_container1.retained_object());
+
+        id retainedObject = objc_container2.retained_object();
+        XCTAssertNotNil(retainedObject);
+        [retainedObject release];
+    }
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
+}
+
+- (void)testMoveConstructor
+{
+    YASObjCContainerTest *objc_object = [[YASObjCContainerTest alloc] init];
+
+    {
+        yas::objc_container objc_container1(objc_object, yas::strong);
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        yas::objc_container objc_container2(std::move(objc_container1));
+
+        XCTAssertEqual([objc_object retainCount], 2);
+
+        XCTAssertNil(objc_container1.retained_object());
+
+        id retainedObject = objc_container2.retained_object();
+        XCTAssertNotNil(retainedObject);
+        [retainedObject release];
+    }
+
+    [objc_object release];
+    objc_object = nil;
+
+    XCTAssertEqual(_objc_object_count, 0);
 }
 
 @end
