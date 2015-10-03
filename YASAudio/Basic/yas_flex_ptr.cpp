5c4887db9ff45d500cc219d36925550d7f3ea904
Yuki Yasoshima <yukiyasos@gmail.com>
2015-10-03 18:22:53 +0900

    add flex_ptr

---
+++ YASAudio/Basic/yas_flex_ptr.cpp
@@ -0,0 +1,32 @@
+//
+//  yas_flex_ptr.cpp
+//  Copyright (c) 2015 Yuki Yasoshima.
+//
+
+#include "yas_flex_ptr.h"
+
+using namespace yas;
+
+flex_ptr::flex_ptr() : v(nullptr)
+{
+}
+
+flex_ptr::flex_ptr(std::nullptr_t) : v(nullptr)
+{
+}
+
+template <typename T>
+flex_ptr::flex_ptr(const T *p)
+    : v(static_cast<void *>(const_cast<T *>(p)))
+{
+}
+
+template yas::flex_ptr::flex_ptr(const void *);
+template yas::flex_ptr::flex_ptr(const Float32 *);
+template yas::flex_ptr::flex_ptr(const Float64 *);
+template yas::flex_ptr::flex_ptr(const SInt32 *);
+template yas::flex_ptr::flex_ptr(const UInt32 *);
+template yas::flex_ptr::flex_ptr(const SInt16 *);
+template yas::flex_ptr::flex_ptr(const UInt16 *);
+template yas::flex_ptr::flex_ptr(const SInt8 *);
+template yas::flex_ptr::flex_ptr(const UInt8 *);
---
+++ YASAudio/Basic/yas_flex_ptr.h
@@ -0,0 +1,33 @@
+//
+//  yas_flex_ptr.h
+//  Copyright (c) 2015 Yuki Yasoshima.
+//
+
+#pragma once
+
+#include <cstddef>
+#include <MacTypes.h>
+
+namespace yas
+{
+    class flex_ptr
+    {
+       public:
+        union {
+            void *v;
+            Float32 *f32;
+            Float64 *f64;
+            SInt32 *i32;
+            UInt32 *u32;
+            SInt16 *i16;
+            UInt16 *u16;
+            SInt8 *i8;
+            UInt8 *u8;
+        };
+
+        flex_ptr();
+        flex_ptr(std::nullptr_t);
+        template <typename T>
+        flex_ptr(const T *p);
+    };
+}
