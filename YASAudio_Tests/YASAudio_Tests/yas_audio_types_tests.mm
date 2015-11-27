9f19d02c39efd208166e5ec6ea3fce68042011b9
Yuki Yasoshima <yukiyasos@gmail.com>
2015-11-28 00:17:58 +0900

    add audio_types tests

--- YASAudio_Tests/YASAudio_Tests/yas_audio_types_tests.mm
+++ YASAudio_Tests/YASAudio_Tests/yas_audio_types_tests.mm
@@ -46,4 +46,53 @@
     XCTAssertEqual(yas::to_string((AudioUnitScope)-1), "unknown");
 }
 
+- (void)test_to_string_render_type
+{
+    XCTAssertEqual(yas::to_string(yas::render_type::normal), "normal");
+    XCTAssertEqual(yas::to_string(yas::render_type::notify), "notify");
+    XCTAssertEqual(yas::to_string(yas::render_type::input), "input");
+    XCTAssertEqual(yas::to_string(yas::render_type::unknown), "unknown");
+}
+
+- (void)test_to_string_audio_error
+{
+    OSStatus err = noErr;
+    XCTAssertEqual(yas::to_string(err), "noErr");
+
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_InvalidProperty), "InvalidProperty");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_InvalidParameter), "InvalidParameter");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_InvalidElement), "InvalidElement");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_NoConnection), "NoConnection");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_FailedInitialization), "FailedInitialization");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_TooManyFramesToProcess), "TooManyFramesToProcess");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_InvalidFile), "InvalidFile");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_FormatNotSupported), "FormatNotSupported");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_Uninitialized), "Uninitialized");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_InvalidScope), "InvalidScope");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_PropertyNotWritable), "PropertyNotWritable");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_CannotDoInCurrentContext), "CannotDoInCurrentContext");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_InvalidPropertyValue), "InvalidPropertyValue");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_PropertyNotInUse), "PropertyNotInUse");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_Initialized), "Initialized");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_InvalidOfflineRender), "InvalidOfflineRender");
+    XCTAssertEqual(yas::to_string(kAudioUnitErr_Unauthorized), "Unauthorized");
+
+#if (TARGET_OS_MAC && !TARGET_OS_IPHONE)
+    XCTAssertEqual(yas::to_string(kAudioHardwareNotRunningError), "HardwareNotRunning");
+    XCTAssertEqual(yas::to_string(kAudioHardwareUnspecifiedError), "HardwareUnspecifiedError");
+    XCTAssertEqual(yas::to_string(kAudioHardwareUnknownPropertyError), "HardwareUnknownPropertyError");
+    XCTAssertEqual(yas::to_string(kAudioHardwareBadPropertySizeError), "HardwareBadPropertySizeError");
+    XCTAssertEqual(yas::to_string(kAudioHardwareIllegalOperationError), "HardwareIllegalOperationError");
+    XCTAssertEqual(yas::to_string(kAudioHardwareBadObjectError), "HardwareBadObjectError");
+    XCTAssertEqual(yas::to_string(kAudioHardwareBadDeviceError), "HardwareBadDeviceError");
+    XCTAssertEqual(yas::to_string(kAudioHardwareBadStreamError), "HardwareBadStreamError");
+    XCTAssertEqual(yas::to_string(kAudioHardwareUnsupportedOperationError), "HardwareUnsupportedOperationError");
+    XCTAssertEqual(yas::to_string(kAudioDeviceUnsupportedFormatError), "DeviceUnsupportedFormatError");
+    XCTAssertEqual(yas::to_string(kAudioDevicePermissionsError), "DevicePermissionsError");
+#endif
+
+    err = 1;
+    XCTAssertEqual(yas::to_string(err), "Unknown");
+}
+
 @end
