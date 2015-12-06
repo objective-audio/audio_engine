ce2364c4b07737da6e4f726b2c274c9cb94f714d
Yuki Yasoshima <yukiyasos@gmail.com>
2015-12-06 13:18:18 +0900

    remove audio:: from audio_device_io_node

--- YASAudio/audio_engine/yas_audio_device_io_node_impl.cpp
+++ YASAudio/audio_engine/yas_audio_device_io_node_impl.cpp
@@ -50,7 +50,7 @@ audio::device_io_node::impl::~impl() = default;
 
 void audio::device_io_node::impl::prepare(const device_io_node &node, const audio::device &device)
 {
-    set_device(device ?: audio::device::default_output_device());
+    set_device(device ?: device::default_output_device());
 }
 
 UInt32 audio::device_io_node::impl::input_bus_count() const
@@ -78,7 +78,7 @@ void audio::device_io_node::impl::update_connections()
     auto weak_node = to_weak(cast<device_io_node>());
     auto weak_device_io = to_weak(device_io);
 
-    auto render_function = [weak_node, weak_device_io](audio::pcm_buffer &output_buffer, const audio::time &when) {
+    auto render_function = [weak_node, weak_device_io](pcm_buffer &output_buffer, const time &when) {
         if (auto node = weak_node.lock()) {
             if (auto kernel = node.impl_ptr<impl>()->kernel_cast()) {
                 if (output_buffer) {
@@ -100,7 +100,7 @@ void audio::device_io_node::impl::update_connections()
                         if (auto destination_node = connection.destination_node()) {
                             if (auto tap_node = destination_node.cast<input_tap_node>()) {
                                 auto input_buffer = device_io.input_buffer_on_render();
-                                const audio::time &input_time = device_io.input_time_on_render();
+                                const auto &input_time = device_io.input_time_on_render();
                                 if (input_buffer && input_time) {
                                     if (connection.format() ==
                                         destination_node.input_format(connection.destination_bus())) {
@@ -176,7 +176,7 @@ audio::device audio::device_io_node::impl::device() const
     return _core->device();
 }
 
-void audio::device_io_node::impl::render(audio::pcm_buffer &buffer, const UInt32 bus_idx, const audio::time &when)
+void audio::device_io_node::impl::render(pcm_buffer &buffer, const UInt32 bus_idx, const time &when)
 {
     super_class::render(buffer, bus_idx, when);
 
