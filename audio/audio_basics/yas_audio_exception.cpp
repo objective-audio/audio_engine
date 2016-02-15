486416e5c39b11c9e846030bf34d7692a1cf9aa8
Yuki Yasoshima <yukiyasos@gmail.com>
2016-02-15 10:44:09 +0900

    update audio_exception

--- audio/audio_basics/yas_audio_exception.cpp
+++ audio/audio_basics/yas_audio_exception.cpp
@@ -3,12 +3,12 @@
 //  Copyright (c) 2015 Yuki Yasoshima.
 //
 
-#include "yas_audio_exception.h"
-#include "yas_audio_types.h"
 #include <exception>
 #include <string>
+#include "yas_audio_exception.h"
+#include "yas_audio_types.h"
 
-void yas::raise_if_au_error(const OSStatus &err) {
+void yas::raise_if_au_error(OSStatus const &err) {
     if (err != noErr) {
         throw std::runtime_error("audio unit error : " + std::to_string(err) + " - " + yas::to_string(err));
     }
