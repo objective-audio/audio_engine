935c2cbff8953766ebd27014f7beb17fc5fa64e5
Yuki Yasoshima <yukiyasos@gmail.com>
2016-02-15 23:48:31 +0900

    update audio_device_io

--- audio/audio_device/yas_audio_device_io.h
+++ audio/audio_device/yas_audio_device_io.h
@@ -8,11 +8,11 @@
 #include <TargetConditionals.h>
 #if (TARGET_OS_MAC && !TARGET_OS_IPHONE)
 
-#include "yas_audio_types.h"
-#include "yas_audio_device.h"
-#include "yas_base.h"
 #include <functional>
 #include <memory>
+#include "yas_audio_device.h"
+#include "yas_audio_types.h"
+#include "yas_base.h"
 
 namespace yas {
 namespace audio {
@@ -23,30 +23,30 @@ namespace audio {
         using super_class = base;
 
        public:
-        using render_f = std::function<void(audio::pcm_buffer &output_buffer, const audio::time &when)>;
+        using render_f = std::function<void(audio::pcm_buffer &output_buffer, audio::time const &when)>;
 
         device_io(std::nullptr_t);
-        explicit device_io(const audio::device &);
+        explicit device_io(audio::device const &);
 
         ~device_io();
 
-        device_io(const device_io &) = default;
+        device_io(device_io const &) = default;
         device_io(device_io &&) = default;
-        device_io &operator=(const device_io &) = default;
+        device_io &operator=(device_io const &) = default;
         device_io &operator=(device_io &&) = default;
 
-        void set_device(const audio::device device);
+        void set_device(audio::device const device);
         audio::device device() const;
         bool is_running() const;
-        void set_render_callback(const render_f &callback);
-        void set_maximum_frames_per_slice(const UInt32 frames);
+        void set_render_callback(render_f const &callback);
+        void set_maximum_frames_per_slice(UInt32 const frames);
         UInt32 maximum_frames_per_slice() const;
 
         void start() const;
         void stop() const;
 
-        const audio::pcm_buffer &input_buffer_on_render() const;
-        const audio::time &input_time_on_render() const;
+        audio::pcm_buffer const &input_buffer_on_render() const;
+        audio::time const &input_time_on_render() const;
 
        private:
         class kernel;
