c5859b47694faecd33788a92bf2bc09939762bbd
Yuki Yasoshima <yukiyasos@gmail.com>
2016-02-16 22:42:34 +0900

    udpate audio_tap_node

--- audio/audio_engine/yas_audio_tap_node.cpp
+++ audio/audio_engine/yas_audio_tap_node.cpp
@@ -13,20 +13,20 @@ audio::tap_node::tap_node() : super_class(std::make_unique<impl>()) {
 audio::tap_node::tap_node(std::nullptr_t) : super_class(nullptr) {
 }
 
-audio::tap_node::tap_node(const std::shared_ptr<impl> &impl) : super_class(impl) {
+audio::tap_node::tap_node(std::shared_ptr<impl> const &impl) : super_class(impl) {
 }
 
 audio::tap_node::~tap_node() = default;
 
-void audio::tap_node::set_render_function(const render_f &func) {
+void audio::tap_node::set_render_function(render_f const &func) {
     impl_ptr<impl>()->set_render_function(func);
 }
 
-audio::connection audio::tap_node::input_connection_on_render(const UInt32 bus_idx) const {
+audio::connection audio::tap_node::input_connection_on_render(UInt32 const bus_idx) const {
     return impl_ptr<impl>()->input_connection_on_render(bus_idx);
 }
 
-audio::connection audio::tap_node::output_connection_on_render(const UInt32 bus_idx) const {
+audio::connection audio::tap_node::output_connection_on_render(UInt32 const bus_idx) const {
     return impl_ptr<impl>()->output_connection_on_render(bus_idx);
 }
 
@@ -38,7 +38,7 @@ audio::connection_smap audio::tap_node::output_connections_on_render() const {
     return impl_ptr<impl>()->output_connections_on_render();
 }
 
-void audio::tap_node::render_source(pcm_buffer &buffer, const UInt32 bus_idx, const time &when) {
+void audio::tap_node::render_source(pcm_buffer &buffer, UInt32 const bus_idx, time const &when) {
     impl_ptr<impl>()->render_source(buffer, bus_idx, when);
 }
 
