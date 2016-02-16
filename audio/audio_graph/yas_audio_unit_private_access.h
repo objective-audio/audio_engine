f059fabadd533d67ef069e1e1091539d46c75991
Yuki Yasoshima <yukiyasos@gmail.com>
2016-02-16 10:52:51 +0900

    update audio_unit_private

--- audio/audio_graph/yas_audio_unit_private_access.h
+++ audio/audio_graph/yas_audio_unit_private_access.h
@@ -17,31 +17,31 @@ class yas::audio::unit::private_access {
         unit.impl_ptr<impl>()->uninitialize();
     }
 
-    static void set_graph_key(unit &unit, const std::experimental::optional<UInt8> &key) {
+    static void set_graph_key(unit &unit, std::experimental::optional<UInt8> const &key) {
         unit.impl_ptr<impl>()->graph_key = key;
     }
 
-    static const std::experimental::optional<UInt8> &graph_key(const unit &unit) {
+    static std::experimental::optional<UInt8> const &graph_key(unit const &unit) {
         return unit.impl_ptr<impl>()->graph_key;
     }
 
-    static void set_key(unit &unit, const std::experimental::optional<UInt16> &key) {
+    static void set_key(unit &unit, std::experimental::optional<UInt16> const &key) {
         unit.impl_ptr<impl>()->key = key;
     }
 
-    static const std::experimental::optional<UInt16> &key(const unit &unit) {
+    static std::experimental::optional<UInt16> const &key(unit const &unit) {
         return unit.impl_ptr<impl>()->key;
     }
 
     template <typename T>
-    static void set_property_data(const unit &unit, const std::vector<T> &data, const AudioUnitPropertyID property_id,
-                                  const AudioUnitScope scope, const AudioUnitElement element) {
+    static void set_property_data(unit const &unit, std::vector<T> const &data, AudioUnitPropertyID const property_id,
+                                  AudioUnitScope const scope, AudioUnitElement const element) {
         unit.impl_ptr<impl>()->set_property_data(data, property_id, scope, element);
     }
 
     template <typename T>
-    static std::vector<T> property_data(const unit &unit, const AudioUnitPropertyID property_id,
-                                        const AudioUnitScope scope, const AudioUnitElement element) {
+    static std::vector<T> property_data(unit const &unit, AudioUnitPropertyID const property_id,
+                                        AudioUnitScope const scope, AudioUnitElement const element) {
         return unit.impl_ptr<impl>()->property_data<T>(property_id, scope, element);
     }
 };
