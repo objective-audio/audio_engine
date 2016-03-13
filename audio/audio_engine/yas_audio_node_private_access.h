1d90479a6c8a68a59e854e24a6a45bf3865049db
Yuki Yasoshima <yukiyasos@gmail.com>
2016-03-13 20:11:05 +0900

    remove comment out

--- audio/audio_engine/yas_audio_node_private_access.h
+++ audio/audio_engine/yas_audio_node_private_access.h
@@ -15,39 +15,7 @@ namespace audio {
         static node create() {
             return node(std::make_shared<node::impl>());
         }
-        /*
-                static audio::connection input_connection(node const &node, UInt32 const bus_idx) {
-                    return node.manageable_node().input_connection(bus_idx);
-                }
 
-                static audio::connection output_connection(node const &node, UInt32 const bus_idx) {
-                    return node.manageable_node().output_connection(bus_idx);
-                }
-
-                static audio::connection_wmap const &input_connections(node const &node) {
-                    return node.manageable_node().input_connections();
-                }
-
-                static audio::connection_wmap const &output_connections(node const &node) {
-                    return node.manageable_node().output_connections();
-                }
-
-                static void add_connection(node &node, audio::connection const &connection) {
-                    node.manageable_node().add_connection(connection);
-                }
-
-                static void remove_connection(node &node, audio::connection const &connection) {
-                    node.manageable_node().remove_connection(connection);
-                }
-
-                static void set_engine(node &node, audio::engine const &engine) {
-                    node.manageable_node().set_engine(engine);
-                }
-
-                static void update_kernel(node &node) {
-                    node.manageable_node().update_kernel();
-                }
-        */
         static std::shared_ptr<kernel> kernel(node const &node) {
             return node.impl_ptr<impl>()->kernel_cast();
         }
