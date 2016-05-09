b4d36f19b6634fdc8fb6821725a50039d1b2c5ea
Yuki Yasoshima <yukiyasos@gmail.com>
2016-05-09 20:16:02 +0900

    replace type in audio_connection_private_access

--- audio/audio_engine/yas_audio_connection_private_access.h
+++ audio/audio_engine/yas_audio_connection_private_access.h
@@ -10,8 +10,8 @@ namespace yas {
 namespace audio {
     class connection::private_access {
        public:
-        static connection create(node &source_node, UInt32 const source_bus, node &destination_node,
-                                 UInt32 const destination_bus, audio::format const &format) {
+        static connection create(node &source_node, uint32_t const source_bus, node &destination_node,
+                                 uint32_t const destination_bus, audio::format const &format) {
             return connection(source_node, source_bus, destination_node, destination_bus, format);
         }
     };
