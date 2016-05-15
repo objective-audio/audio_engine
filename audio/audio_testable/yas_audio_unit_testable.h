78b61d0029994ebb078f927f07e2106cf3cb3d34
Yuki Yasoshima <yukiyasos@gmail.com>
2016-05-15 11:04:36 +0900

    testable_unit

--- audio/audio_testable/yas_audio_unit_testable.h
+++ audio/audio_testable/yas_audio_unit_testable.h
@@ -6,19 +6,23 @@
 
 #if YAS_TEST
 
-class yas::audio::unit::testable {
-   public:
-    template <typename T>
-    static void set_property_data(unit const &unit, std::vector<T> const &data, AudioUnitPropertyID const property_id,
-                                  AudioUnitScope const scope, AudioUnitElement const element) {
-        unit.impl_ptr<impl>()->set_property_data(data, property_id, scope, element);
-    }
+namespace yas {
+namespace audio {
+    struct testable_unit {
+        template <typename T>
+        static void set_property_data(unit const &unit, std::vector<T> const &data,
+                                      AudioUnitPropertyID const property_id, AudioUnitScope const scope,
+                                      AudioUnitElement const element) {
+            unit.impl_ptr<audio::unit::impl>()->set_property_data(data, property_id, scope, element);
+        }
 
-    template <typename T>
-    static std::vector<T> property_data(unit const &unit, AudioUnitPropertyID const property_id,
-                                        AudioUnitScope const scope, AudioUnitElement const element) {
-        return unit.impl_ptr<impl>()->property_data<T>(property_id, scope, element);
-    }
-};
+        template <typename T>
+        static std::vector<T> property_data(unit const &unit, AudioUnitPropertyID const property_id,
+                                            AudioUnitScope const scope, AudioUnitElement const element) {
+            return unit.impl_ptr<audio::unit::impl>()->property_data<T>(property_id, scope, element);
+        }
+    };
+}
+}
 
 #endif
