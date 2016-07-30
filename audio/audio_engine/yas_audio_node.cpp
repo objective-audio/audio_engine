99a4d35a3f4661537e23a658ca7abdda0e00712b
Yuki Yasoshima <yukiyasos@gmail.com>
2016-07-30 17:28:07 +0900

    audio_node init

--- audio/audio_engine/yas_audio_node.cpp
+++ audio/audio_engine/yas_audio_node.cpp
@@ -11,6 +11,11 @@
 
 using namespace yas;
 
+audio::node::node(args const args) : base(std::make_shared<impl>()) {
+    impl_ptr<impl>()->set_input_bus_count(args.input_bus_count);
+    impl_ptr<impl>()->set_output_bus_count(args.output_bus_count);
+}
+
 audio::node::node(std::nullptr_t) : base(nullptr) {
 }
 
