f4af7201c72e7674e7dccf320362a7b16cc81665
Yuki Yasoshima <yukiyasos@gmail.com>
2017-04-29 11:16:07 +0900

    make const each_dat

--- audio/audio_basics/yas_audio_each_data_private.h
+++ audio/audio_basics/yas_audio_each_data_private.h
@@ -16,12 +16,27 @@ namespace audio {
         auto const buffer_count = format.buffer_count();
 
         std::vector<T *> vec;
-        vec.resize(buffer_count);
+        vec.reserve(buffer_count);
 
         auto each = make_fast_each(buffer_count);
         while (yas_each_next(each)) {
-            auto const &idx = yas_each_index(each);
-            vec[idx] = buffer.data_ptr_at_index<T>(idx);
+            vec.push_back(buffer.data_ptr_at_index<T>(yas_each_index(each)));
+        }
+
+        return yas::make_each_data<T>(vec.data(), buffer.frame_length(), format.buffer_count(), format.stride());
+    }
+
+    template <typename T>
+    const_each_data<T> make_each_data(pcm_buffer const &buffer) {
+        auto const &format = buffer.format();
+        auto const buffer_count = format.buffer_count();
+
+        std::vector<T const *> vec;
+        vec.reserve(buffer_count);
+
+        auto each = make_fast_each(buffer_count);
+        while (yas_each_next(each)) {
+            vec.push_back(buffer.data_ptr_at_index<T>(yas_each_index(each)));
         }
 
         return yas::make_each_data<T>(vec.data(), buffer.frame_length(), format.buffer_count(), format.stride());
