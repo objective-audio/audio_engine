22dc10f05f00f0d148b1c75f45517b0ae544d9b4
Yuki Yasoshima <yukiyasos@gmail.com>
2017-04-29 11:43:15 +0900

    remove enumerator

--- audio/yas_audio_cpp.h
+++ audio/yas_audio_cpp.h
@@ -18,7 +18,6 @@
 #include "yas_audio_time.h"
 #include "yas_audio_format.h"
 #include "yas_audio_pcm_buffer.h"
-#include "yas_audio_enumerator.h"
 #include "yas_audio_each_data.h"
 #include "yas_audio_file.h"
 #include "yas_audio_file_utils.h"
