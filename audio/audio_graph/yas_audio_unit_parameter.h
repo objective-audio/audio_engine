1108c35375bc63ddf301f9007da460ea9d9e1321
Yuki Yasoshima <yukiyasos@gmail.com>
2018-02-15 09:36:11 +0900

    audio_graph namespace

--- audio/audio_graph/yas_audio_unit_parameter.h
+++ audio/audio_graph/yas_audio_unit_parameter.h
@@ -12,46 +12,48 @@ template <typename T, typename K>
 class subject;
 template <typename T, typename K>
 class observer;
-
-namespace audio {
-    class unit::parameter : public base {
-       public:
-        class impl;
-
-        struct change_info {
-            parameter const &parameter;
-            AudioUnitElement const element;
-            AudioUnitParameterValue const old_value;
-            AudioUnitParameterValue const new_value;
-        };
-
-        enum class method { will_change, did_change };
-
-        using subject_t = subject<method, change_info>;
-        using observer_t = observer<method, change_info>;
-
-        parameter(AudioUnitParameterInfo const &info, AudioUnitParameterID const paramter_id,
-                  AudioUnitScope const scope);
-        parameter(std::nullptr_t);
-
-        AudioUnitParameterID parameter_id() const;
-        AudioUnitScope scope() const;
-        CFStringRef unit_name() const;
-        bool has_clump() const;
-        uint32_t clump_id() const;
-        CFStringRef name() const;
-        AudioUnitParameterUnit unit() const;
-        AudioUnitParameterValue min_value() const;
-        AudioUnitParameterValue max_value() const;
-        AudioUnitParameterValue default_value() const;
-
-        float value(AudioUnitElement const element) const;
-        void set_value(float const value, AudioUnitElement const element);
-        std::unordered_map<AudioUnitElement, AudioUnitParameterValue> const &values() const;
-
-        subject_t &subject();
-    };
 }
+
+namespace yas::audio {
+class unit::parameter : public base {
+   public:
+    class impl;
+
+    struct change_info {
+        parameter const &parameter;
+        AudioUnitElement const element;
+        AudioUnitParameterValue const old_value;
+        AudioUnitParameterValue const new_value;
+    };
+
+    enum class method { will_change, did_change };
+
+    using subject_t = subject<method, change_info>;
+    using observer_t = observer<method, change_info>;
+
+    parameter(AudioUnitParameterInfo const &info, AudioUnitParameterID const paramter_id, AudioUnitScope const scope);
+    parameter(std::nullptr_t);
+
+    AudioUnitParameterID parameter_id() const;
+    AudioUnitScope scope() const;
+    CFStringRef unit_name() const;
+    bool has_clump() const;
+    uint32_t clump_id() const;
+    CFStringRef name() const;
+    AudioUnitParameterUnit unit() const;
+    AudioUnitParameterValue min_value() const;
+    AudioUnitParameterValue max_value() const;
+    AudioUnitParameterValue default_value() const;
+
+    float value(AudioUnitElement const element) const;
+    void set_value(float const value, AudioUnitElement const element);
+    std::unordered_map<AudioUnitElement, AudioUnitParameterValue> const &values() const;
+
+    subject_t &subject();
+};
+}
+
+namespace yas {
 std::string to_string(audio::unit::parameter::method const &);
 }
 
--- audio/audio_graph/yas_audio_unit_protocol.h
+++ audio/audio_graph/yas_audio_unit_protocol.h
@@ -1,3 +1,4 @@
+
 //
 //  yas_audio_unit_protocol.h
 //
@@ -7,27 +8,25 @@
 #include <experimental/optional>
 #include "yas_protocol.h"
 
-namespace yas {
-namespace audio {
-    struct manageable_unit : protocol {
-        struct impl : protocol::impl {
-            virtual void initialize() = 0;
-            virtual void uninitialize() = 0;
-            virtual void set_graph_key(std::experimental::optional<uint8_t> const &) = 0;
-            virtual std::experimental::optional<uint8_t> const &graph_key() const = 0;
-            virtual void set_key(std::experimental::optional<uint16_t> const &) = 0;
-            virtual std::experimental::optional<uint16_t> const &key() const = 0;
-        };
-
-        explicit manageable_unit(std::shared_ptr<impl> impl);
-        manageable_unit(std::nullptr_t);
-
-        void initialize();
-        void uninitialize();
-        void set_graph_key(std::experimental::optional<uint8_t> const &key);
-        std::experimental::optional<uint8_t> const &graph_key() const;
-        void set_key(std::experimental::optional<uint16_t> const &key);
-        std::experimental::optional<uint16_t> const &key() const;
+namespace yas::audio {
+struct manageable_unit : protocol {
+    struct impl : protocol::impl {
+        virtual void initialize() = 0;
+        virtual void uninitialize() = 0;
+        virtual void set_graph_key(std::experimental::optional<uint8_t> const &) = 0;
+        virtual std::experimental::optional<uint8_t> const &graph_key() const = 0;
+        virtual void set_key(std::experimental::optional<uint16_t> const &) = 0;
+        virtual std::experimental::optional<uint16_t> const &key() const = 0;
     };
-}
+
+    explicit manageable_unit(std::shared_ptr<impl> impl);
+    manageable_unit(std::nullptr_t);
+
+    void initialize();
+    void uninitialize();
+    void set_graph_key(std::experimental::optional<uint8_t> const &key);
+    std::experimental::optional<uint8_t> const &graph_key() const;
+    void set_key(std::experimental::optional<uint16_t> const &key);
+    std::experimental::optional<uint16_t> const &key() const;
+};
 }
