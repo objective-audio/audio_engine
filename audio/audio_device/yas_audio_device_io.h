bfbcc6d5aa669d5fa12cf1bc963ed9619165dc68
Yuki Yasoshima <yukiyasos@gmail.com>
2018-02-15 09:33:40 +0900

    audio_device namespace

--- audio/audio_device/yas_audio_device_io.h
+++ audio/audio_device/yas_audio_device_io.h
@@ -11,45 +11,43 @@
 #include "yas_audio_types.h"
 #include "yas_base.h"
 
-namespace yas {
-namespace audio {
-    class pcm_buffer;
-    class time;
-    class device;
+namespace yas::audio {
+class pcm_buffer;
+class time;
+class device;
 
-    class device_io : public base {
-       public:
-        struct render_args {
-            audio::pcm_buffer &output_buffer;
-            audio::time const &when;
-        };
-
-        using render_f = std::function<void(render_args)>;
-
-        device_io(std::nullptr_t);
-        explicit device_io(audio::device const &);
-
-        void set_device(audio::device const device);
-        audio::device device() const;
-        bool is_running() const;
-        void set_render_handler(render_f);
-        void set_maximum_frames_per_slice(uint32_t const frames);
-        uint32_t maximum_frames_per_slice() const;
-
-        void start() const;
-        void stop() const;
-
-        audio::pcm_buffer const &input_buffer_on_render() const;
-        audio::time const &input_time_on_render() const;
-
-       private:
-        class kernel;
-        class impl;
-
-        void _initialize() const;
-        void _uninitialize() const;
+class device_io : public base {
+   public:
+    struct render_args {
+        audio::pcm_buffer &output_buffer;
+        audio::time const &when;
     };
-}
+
+    using render_f = std::function<void(render_args)>;
+
+    device_io(std::nullptr_t);
+    explicit device_io(audio::device const &);
+
+    void set_device(audio::device const device);
+    audio::device device() const;
+    bool is_running() const;
+    void set_render_handler(render_f);
+    void set_maximum_frames_per_slice(uint32_t const frames);
+    uint32_t maximum_frames_per_slice() const;
+
+    void start() const;
+    void stop() const;
+
+    audio::pcm_buffer const &input_buffer_on_render() const;
+    audio::time const &input_time_on_render() const;
+
+   private:
+    class kernel;
+    class impl;
+
+    void _initialize() const;
+    void _uninitialize() const;
+};
 }
 
 template <>
--- audio/audio_device/yas_audio_route.h
+++ audio/audio_device/yas_audio_route.h
@@ -10,35 +10,34 @@
 namespace yas {
 template <typename T, typename U>
 class result;
+}
 
-namespace audio {
-    struct route {
-        struct point {
-            uint32_t bus;
-            uint32_t channel;
+namespace yas::audio {
+struct route {
+    struct point {
+        uint32_t bus;
+        uint32_t channel;
 
-            bool operator==(point const &) const;
-            bool operator!=(point const &) const;
-        };
-
-        route(uint32_t const src_bus_idx, uint32_t const src_ch_idx, uint32_t const dst_bus_idx,
-              uint32_t const dst_ch_idx);
-        route(uint32_t const bus_idx, uint32_t const ch_idx);
-        route(point const &src_point, point const &dst_point);
-
-        bool operator==(route const &) const;
-        bool operator!=(route const &) const;
-        bool operator<(route const &) const;
-
-        point source;
-        point destination;
+        bool operator==(point const &) const;
+        bool operator!=(point const &) const;
     };
 
-    using route_set_t = std::set<route>;
+    route(uint32_t const src_bus_idx, uint32_t const src_ch_idx, uint32_t const dst_bus_idx, uint32_t const dst_ch_idx);
+    route(uint32_t const bus_idx, uint32_t const ch_idx);
+    route(point const &src_point, point const &dst_point);
 
-    using channel_map_result = result<channel_map_t, std::nullptr_t>;
-    channel_map_result channel_map_from_routes(route_set_t const &routes, uint32_t const src_bus_idx,
-                                               uint32_t const src_ch_count, uint32_t const dst_bus_idx,
-                                               uint32_t const dst_ch_count);
-}
+    bool operator==(route const &) const;
+    bool operator!=(route const &) const;
+    bool operator<(route const &) const;
+
+    point source;
+    point destination;
+};
+
+using route_set_t = std::set<route>;
+
+using channel_map_result = result<channel_map_t, std::nullptr_t>;
+channel_map_result channel_map_from_routes(route_set_t const &routes, uint32_t const src_bus_idx,
+                                           uint32_t const src_ch_count, uint32_t const dst_bus_idx,
+                                           uint32_t const dst_ch_count);
 }
