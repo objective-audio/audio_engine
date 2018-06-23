089a87460a9fe0574e84eb65ee7b262d93856b66
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-23 20:22:28 +0900

    fix tests

--- audio_tests/audio_tests/audio_device_tests/yas_audio_device_stream_tests.mm
+++ audio_tests/audio_tests/audio_device_tests/yas_audio_device_stream_tests.mm
@@ -2,10 +2,14 @@
 //  yas_audio_device_stream_tests.m
 //
 
+#include <TargetConditionals.h>
+
 #if (TARGET_OS_MAC && !TARGET_OS_IPHONE)
 
 #import "yas_audio_device_stream.h"
 #import "yas_audio_test_utils.h"
+
+using namespace yas;
 
 @interface yas_audio_device_stream_tests : XCTestCase
 
