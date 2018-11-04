b57da083df0883423d5ad6d7335dd51e1a315f52
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-04 12:16:32 +0900

    update format.sh

--- script/format.sh
+++ script/format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clang-format -i -style=file `find ../audio ../audio_sample_ios ../audio_sample_mac -type f \( -name *.h -o -name *.cpp -o -name *.hpp -o -name *.m -o -name *.mm \)`
+clang-format -i -style=file `find ../audio ../audio_sample_ios ../audio_sample_mac ../audio_tests -type f \( -name *.h -o -name *.cpp -o -name *.hpp -o -name *.m -o -name *.mm \)`
