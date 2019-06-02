b63d03d7413269aacb92352f15fd703ab66783b3
Yuki Yasoshima <yukiyasos@gmail.com>
2019-06-02 14:20:57 +0900

    update umbrella

--- audio_ios/audio/audio.h
+++ audio_ios/audio/audio.h
@@ -11,3 +11,6 @@ FOUNDATION_EXPORT double audioVersionNumber;
 FOUNDATION_EXPORT const unsigned char audioVersionString[];
 
 #import <audio/yas_audio_umbrella.h>
+#import <cpp_utils/yas_objc_ptr.h>
+#import <objc_utils/yas_objc_macros.h>
+#import <audio/yas_audio_objc_utils.h>
