d0217b1a9b9f8fc21f8247694e3ad62faa3a2541
Yuki Yasoshima <yukiyasos@gmail.com>
2019-07-19 23:22:13 +0900

    au_mixer

--- audio/audio_engine/yas_audio_engine_au_mixer.cpp
+++ audio/audio_engine/yas_audio_engine_au_mixer.cpp
@@ -22,43 +22,43 @@ audio::engine::au_mixer::au_mixer()
 }
 
 void audio::engine::au_mixer::set_output_volume(float const volume, uint32_t const bus_idx) {
-    this->au().set_output_parameter_value(kMultiChannelMixerParam_Volume, volume, bus_idx);
+    this->_au->set_output_parameter_value(kMultiChannelMixerParam_Volume, volume, bus_idx);
 }
 
 float audio::engine::au_mixer::output_volume(uint32_t const bus_idx) const {
-    return this->au().output_parameter_value(kMultiChannelMixerParam_Volume, bus_idx);
+    return this->_au->output_parameter_value(kMultiChannelMixerParam_Volume, bus_idx);
 }
 
 void audio::engine::au_mixer::set_output_pan(float const pan, uint32_t const bus_idx) {
-    this->au().set_output_parameter_value(kMultiChannelMixerParam_Pan, pan, bus_idx);
+    this->_au->set_output_parameter_value(kMultiChannelMixerParam_Pan, pan, bus_idx);
 }
 
 float audio::engine::au_mixer::output_pan(uint32_t const bus_idx) const {
-    return this->au().output_parameter_value(kMultiChannelMixerParam_Pan, bus_idx);
+    return this->_au->output_parameter_value(kMultiChannelMixerParam_Pan, bus_idx);
 }
 
 void audio::engine::au_mixer::set_input_volume(float const volume, uint32_t const bus_idx) {
-    this->au().set_input_parameter_value(kMultiChannelMixerParam_Volume, volume, bus_idx);
+    this->_au->set_input_parameter_value(kMultiChannelMixerParam_Volume, volume, bus_idx);
 }
 
 float audio::engine::au_mixer::input_volume(uint32_t const bus_idx) const {
-    return this->au().input_parameter_value(kMultiChannelMixerParam_Volume, bus_idx);
+    return this->_au->input_parameter_value(kMultiChannelMixerParam_Volume, bus_idx);
 }
 
 void audio::engine::au_mixer::set_input_pan(float const pan, uint32_t const bus_idx) {
-    this->au().set_input_parameter_value(kMultiChannelMixerParam_Pan, pan, bus_idx);
+    this->_au->set_input_parameter_value(kMultiChannelMixerParam_Pan, pan, bus_idx);
 }
 
 float audio::engine::au_mixer::input_pan(uint32_t const bus_idx) const {
-    return this->au().input_parameter_value(kMultiChannelMixerParam_Pan, bus_idx);
+    return this->_au->input_parameter_value(kMultiChannelMixerParam_Pan, bus_idx);
 }
 
 void audio::engine::au_mixer::set_input_enabled(bool const enabled, uint32_t const bus_idx) {
-    this->au().set_input_parameter_value(kMultiChannelMixerParam_Enable, enabled ? 1.0f : 0.0f, bus_idx);
+    this->_au->set_input_parameter_value(kMultiChannelMixerParam_Enable, enabled ? 1.0f : 0.0f, bus_idx);
 }
 
 bool audio::engine::au_mixer::input_enabled(uint32_t const bus_idx) const {
-    return this->au().input_parameter_value(kMultiChannelMixerParam_Enable, bus_idx) != 0.0f;
+    return this->_au->input_parameter_value(kMultiChannelMixerParam_Enable, bus_idx) != 0.0f;
 }
 
 audio::engine::au const &audio::engine::au_mixer::au() const {
