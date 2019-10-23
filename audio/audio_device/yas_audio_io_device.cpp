612a572597c1ef9811cd6213a887dcd7cede9d5e
Yuki Yasoshima <yukiyasos@gmail.com>
2019-10-23 22:29:40 +0900

    defaut_device

---
+++ audio/audio_device/yas_audio_io_device.cpp
@@ -0,0 +1,21 @@
+//
+//  yas_audio_io_device.cpp
+//
+
+#include "yas_audio_io_device.h"
+
+#if TARGET_OS_IPHONE
+#include "yas_audio_avf_device.h"
+#elif TARGET_OS_MAC
+#include "yas_audio_mac_device.h"
+#endif
+
+using namespace yas;
+
+std::optional<audio::io_device_ptr> audio::io_device::default_device() {
+#if TARGET_OS_IPHONE
+    return avf_device::make_shared();
+#elif TARGET_OS_MAC
+    return mac_device::default_output_device();
+#endif
+}
