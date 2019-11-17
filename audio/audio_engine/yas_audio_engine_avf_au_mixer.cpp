e5ecdc9e8a7579b5da9076f954fb638827997cf1
Yuki Yasoshima <yukiyasos@gmail.com>
2019-11-17 13:17:53 +0900

    avf_au_mixer

--- audio/audio_engine/yas_audio_engine_avf_au_mixer.cpp
+++ audio/audio_engine/yas_audio_engine_avf_au_mixer.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "yas_audio_engine_avf_au_mixer.h"
+
+using namespace yas;
+
+audio::engine::avf_au_mixer::avf_au_mixer()
+    : _au(avf_au::make_shared(
+          {.acd =
+               AudioComponentDescription{
+                   .componentType = kAudioUnitType_Mixer,
+                   .componentSubType = kAudioUnitSubType_MultiChannelMixer,
+                   .componentManufacturer = kAudioUnitManufacturer_Apple,
+                   .componentFlags = 0,
+                   .componentFlagsMask = 0,
+               },
+           .node_args = {.input_bus_count = std::numeric_limits<uint32_t>::max(), .output_bus_count = 1}})) {
+}
+
+void audio::engine::avf_au_mixer::_prepare(avf_au_mixer_ptr const &shared) {
+}
+
+void audio::engine::avf_au_mixer::_update_unit_mixer_connections() {
+    auto const &connections = manageable_node::cast(this->_au->node())->input_connections();
+    if (connections.size() > 0) {
+        auto last = connections.end();
+        --last;
+
+        auto &pair = *last;
+        this->_au->set_input_bus_count(pair.first + 1);
+    }
+}
+
+audio::engine::avf_au_mixer_ptr audio::engine::avf_au_mixer::make_shared() {
+    auto shared = std::shared_ptr<avf_au_mixer>(new avf_au_mixer{});
+    return shared;
+}
--- audio/audio_engine/yas_audio_engine_avf_au_mixer.h
+++ audio/audio_engine/yas_audio_engine_avf_au_mixer.h
@@ -3,3 +3,20 @@
 //
 
 #pragma once
+
+#include "yas_audio_engine_avf_au.h"
+
+namespace yas::audio::engine {
+struct avf_au_mixer final {
+    static avf_au_mixer_ptr make_shared();
+
+   private:
+    avf_au_ptr _au;
+    std::optional<chaining::any_observer_ptr> _connections_observer = std::nullopt;
+
+    avf_au_mixer();
+
+    void _prepare(avf_au_mixer_ptr const &);
+    void _update_unit_mixer_connections();
+};
+}  // namespace yas::audio::engine
