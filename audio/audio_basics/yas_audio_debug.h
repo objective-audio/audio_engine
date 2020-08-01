5e7951a234a2c146eb6f1303f9cade1335288171
Yuki Yasoshima <yukiyasos@gmail.com>
2020-08-01 22:19:00 +0900

    add debug

---
+++ audio/audio_basics/yas_audio_debug.h
@@ -0,0 +1,22 @@
+//
+//  yas_audio_debug.h
+//
+
+#pragma once
+
+#if DEBUG
+
+#include <string>
+
+namespace yas::audio {
+void set_log_enabled(bool const);
+void log(std::string const &);
+}  // namespace yas::audio
+
+#define yas_audio_log(__v) yas::audio::log(__v)
+
+#else
+
+#define yas_audio_log(__v)
+
+#endif
