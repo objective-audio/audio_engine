edc5733eceeb9882be2a2b92194a45ce90f9eec9
Yuki Yasoshima <yukiyasos@gmail.com>
2020-08-04 21:45:24 +0900

    convert format

--- audio/audio_basics/yas_audio_objc_utils.h
+++ audio/audio_basics/yas_audio_objc_utils.h
@@ -10,12 +10,16 @@
 
 namespace yas::audio {
 class time;
+class format;
 }
 
 namespace yas {
 #if TARGET_OS_IPHONE
 audio::channel_map_t to_channel_map(NSArray *const channelDescriptions, audio::direction const dir);
 #endif
+
+AVAudioCommonFormat to_common_format(audio::pcm_format const);
+objc_ptr<AVAudioFormat *> to_objc_object(audio::format const &);
 
 objc_ptr<AVAudioTime *> to_objc_object(audio::time const &time);
 audio::time to_time(AVAudioTime *const av_time);
