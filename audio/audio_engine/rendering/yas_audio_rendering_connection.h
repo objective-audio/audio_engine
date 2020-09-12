5251399381c6acae7ca6299731fbdeb9e80aebd2
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-12 14:05:33 +0900

    input

--- audio/audio_engine/rendering/yas_audio_rendering_connection.h
+++ audio/audio_engine/rendering/yas_audio_rendering_connection.h
@@ -4,10 +4,16 @@
 
 #pragma once
 
+#include <unordered_map>
 #include <unordered_set>
 
 namespace yas::audio {
-struct rendering_connection {};
+class rendering_node;
+
+struct rendering_connection {
+    uint32_t const input_bus_idx;
+    rendering_node *const input_node;
+};
 
 using rendering_connection_set = std::unordered_set<rendering_connection>;
 }  // namespace yas::audio
--- audio/audio_engine/rendering/yas_audio_rendering_node.h
+++ audio/audio_engine/rendering/yas_audio_rendering_node.h
@@ -4,10 +4,15 @@
 
 #pragma once
 
+#include <unordered_map>
 #include <unordered_set>
 
 namespace yas::audio {
-struct rendering_node {};
+class rendering_connection;
+
+struct rendering_node {
+    std::unordered_map<uint32_t, rendering_connection *> const input_connections;
+};
 
 using rendering_node_set = std::unordered_set<rendering_node>;
 }  // namespace yas::audio
