d77177820d3528396c48d354926260d8b3b86105
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-12 13:56:29 +0900

    add test file

---
+++ audio_tests/rendering_tests/yas_audio_rendering_tests.mm
@@ -0,0 +1,14 @@
+//
+//  yas_audio_rendering_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#include <audio/yas_audio_rendering_graph.h>
+
+@interface yas_audio_rendering_tests : XCTestCase
+
+@end
+
+@implementation yas_audio_rendering_tests
+
+@end
