c5a7b5e09cf0eb8e855a9cc7cf86ae7d7de2874b
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-12 16:28:53 +0900

    render

--- audio/audio_engine/rendering/yas_audio_rendering_connection.cpp
+++ audio/audio_engine/rendering/yas_audio_rendering_connection.cpp
@@ -4,8 +4,13 @@
 
 #include "yas_audio_rendering_connection.h"
 
+#include <audio/yas_audio_rendering_node.h>
+
 using namespace yas;
 
-void audio::rendering_connection::render(audio::pcm_buffer *const, audio::time const &) {
-#warning todo
+void audio::rendering_connection::render(audio::pcm_buffer *const buffer, audio::time const &time) {
+    this->input_node->render({.buffer = buffer,
+                              .bus_idx = this->input_bus_idx,
+                              .time = time,
+                              .input_connections = this->input_node->input_connections});
 }
