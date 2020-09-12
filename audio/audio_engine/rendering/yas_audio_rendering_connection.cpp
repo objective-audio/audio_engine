20d2b97a42939b1822044c939b3a0a920d6581dd
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-12 16:23:33 +0900

    render

--- audio/audio_engine/rendering/yas_audio_rendering_connection.cpp
+++ audio/audio_engine/rendering/yas_audio_rendering_connection.cpp
@@ -3,3 +3,8 @@
 //
 
 #include "yas_audio_rendering_connection.h"
+
+using namespace yas;
+
+void audio::rendering_connection::render(audio::pcm_buffer *const, audio::time const &) {
+}
--- audio/audio_engine/rendering/yas_audio_rendering_connection.h
+++ audio/audio_engine/rendering/yas_audio_rendering_connection.h
@@ -4,6 +4,9 @@
 
 #pragma once
 
+#include <audio/yas_audio_pcm_buffer.h>
+#include <audio/yas_audio_time.h>
+
 #include <unordered_map>
 #include <unordered_set>
 
@@ -13,6 +16,8 @@ class rendering_node;
 struct rendering_connection {
     uint32_t const input_bus_idx;
     rendering_node *const input_node;
+
+    void render(audio::pcm_buffer *const, audio::time const &);
 };
 
 using rendering_connection_set = std::unordered_set<rendering_connection>;
