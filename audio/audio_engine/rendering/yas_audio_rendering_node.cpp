12306e39777b0fa018e7635763db407e544d086b
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-12 22:37:03 +0900

    connection

--- audio/audio_engine/rendering/yas_audio_rendering_node.cpp
+++ audio/audio_engine/rendering/yas_audio_rendering_node.cpp
@@ -6,6 +6,9 @@
 
 using namespace yas;
 
-void audio::rendering_node::render(audio::pcm_buffer *const buffer, uint32_t const bus_idx, audio::time const &time) {
-    this->render_handler({.buffer = buffer, .bus_idx = bus_idx, .time = time, .input_nodes = this->input_nodes});
+void audio::rendering_connection::render(audio::pcm_buffer *const buffer, audio::time const &time) const {
+    this->source_node->render_handler({.buffer = buffer,
+                                       .bus_idx = this->source_bus_idx,
+                                       .time = time,
+                                       .source_connections = this->source_node->source_connections});
 }
--- audio/audio_engine/rendering/yas_audio_rendering_node.h
+++ audio/audio_engine/rendering/yas_audio_rendering_node.h
@@ -11,23 +11,30 @@
 #include <unordered_set>
 
 namespace yas::audio {
+class rendering_node;
+
+struct rendering_connection {
+    uint32_t const source_bus_idx;
+    rendering_node const *const source_node;
+
+    void render(audio::pcm_buffer *const, audio::time const &) const;
+};
+
 struct rendering_node {
-    using node_map = std::unordered_map<uint32_t, rendering_node const *>;
+    using connection_map = std::unordered_map<uint32_t, rendering_connection>;
 
     struct render_args {
         audio::pcm_buffer *const buffer;
         uint32_t const bus_idx;
         audio::time const &time;
 
-        node_map const &input_nodes;
+        connection_map const &source_connections;
     };
 
     using render_f = std::function<void(render_args const &)>;
 
     render_f const render_handler;
-    node_map const input_nodes;
-
-    void render(audio::pcm_buffer *const, uint32_t const bus_idx, audio::time const &) const;
+    connection_map const source_connections;
 
    private:
     rendering_node(rendering_node const &) = delete;
