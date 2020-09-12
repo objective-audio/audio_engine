ac25f4c6d7a0d482a2736a00a82936809a518afd
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-12 22:52:28 +0900

    tests

--- audio_tests/rendering_tests/yas_audio_rendering_tests.mm
+++ audio_tests/rendering_tests/yas_audio_rendering_tests.mm
@@ -26,22 +26,40 @@ using namespace yas;
     std::vector<called_node> called;
 
     audio::rendering_node const input_node_0{
-        .render_handler = [&called](auto const &) { called.emplace_back(called_node::input_0); }, .input_nodes = {}};
+        .render_handler = [&called](auto const &args) { called.emplace_back(called_node::input_0); },
+        .source_connections = {}};
     audio::rendering_node const input_node_1{
-        .render_handler = [&called](auto const &) { called.emplace_back(called_node::input_1); }, .input_nodes = {}};
-    audio::rendering_node const input_node_2{
-        .render_handler = [&called](auto const &) { called.emplace_back(called_node::input_2); }, .input_nodes = {}};
+        .render_handler = [&called](auto const &args) { called.emplace_back(called_node::input_1); },
+        .source_connections = {}};
 
     audio::rendering_node const middle_node_0{
-        .render_handler = [&called](auto const &) { called.emplace_back(called_node::middle_0); },
-        .input_nodes = {{0, &input_node_0}}};
+        .render_handler = [&called](auto const &args) { called.emplace_back(called_node::middle_0); },
+        .source_connections = {{0, {.source_bus_idx = 0, .source_node = &input_node_0}}}};
     audio::rendering_node const middle_node_1{
-        .render_handler = [&called](auto const &) { called.emplace_back(called_node::middle_1); },
-        .input_nodes = {{1, &input_node_1}, {2, &input_node_2}}};
+        .render_handler = [&called](auto const &args) { called.emplace_back(called_node::middle_1); },
+        .source_connections = {{0, {.source_bus_idx = 0, .source_node = &input_node_1}},
+                               {1, {.source_bus_idx = 1, .source_node = &input_node_1}}}};
 
     audio::rendering_node const last_node{
-        .render_handler = [&called](auto const &) { called.emplace_back(called_node::last); },
-        .input_nodes = {{0, &middle_node_0}, {1, &middle_node_1}}};
+        .render_handler =
+            [&called](auto const &args) {
+                called.emplace_back(called_node::last);
+                for (auto [bus_idx, connection] : args.source_connections) {
+                }
+            },
+        .source_connections = {{0, {.source_bus_idx = 0, .source_node = &middle_node_0}},
+                               {1, {.source_bus_idx = 0, .source_node = &middle_node_1}}}};
+
+    XCTAssertEqual(called.size(), 0);
+
+    audio::format format{{.sample_rate = 2, .channel_count = 1}};
+    audio::pcm_buffer buffer{format, 2};
+    audio::time time{0};
+
+    last_node.render_handler(
+        {.buffer = &buffer, .bus_idx = 0, .time = time, .source_connections = last_node.source_connections});
+
+    XCTAssertEqual(called.size(), 1);
 }
 
 @end
