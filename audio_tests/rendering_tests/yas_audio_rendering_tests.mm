0b2e150534b3c28fccf8ebeb6d7cc49d9b32c9f6
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-12 18:03:40 +0900

    tests

--- audio_tests/rendering_tests/yas_audio_rendering_tests.mm
+++ audio_tests/rendering_tests/yas_audio_rendering_tests.mm
@@ -5,10 +5,19 @@
 #import <XCTest/XCTest.h>
 #include <audio/yas_audio_rendering_graph.h>
 
+using namespace yas;
+
 @interface yas_audio_rendering_tests : XCTestCase
 
 @end
 
 @implementation yas_audio_rendering_tests
+
+- (void)test_render {
+    audio::rendering_node last_node{.render_handler = [](auto const &) {}, .input_nodes = {}};
+    audio::rendering_node middle_node{.render_handler = [](auto const &) {}, .input_nodes = {}};
+    audio::rendering_node input_node_0{.render_handler = [](auto const &) {}, .input_nodes = {}};
+    audio::rendering_node input_node_1{.render_handler = [](auto const &) {}, .input_nodes = {}};
+}
 
 @end
