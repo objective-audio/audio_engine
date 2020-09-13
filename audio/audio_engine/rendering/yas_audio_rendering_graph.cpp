a9707d185dad26a51cb074adfc8f3ba641ea41e5
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-13 13:50:01 +0900

    construct rendering_graph

--- audio/audio_engine/rendering/yas_audio_rendering_graph.cpp
+++ audio/audio_engine/rendering/yas_audio_rendering_graph.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "yas_audio_rendering_graph.h"
+
+using namespace yas;
+
+namespace yas::audio {
+std::vector<rendering_node> make_rendering_nodes(graph_node_set const &nodes, graph_connection_set const &connections) {
+#warning todo
+    return {};
+}
+}  // namespace yas::audio
+
+audio::rendering_graph::rendering_graph(graph_node_set const &nodes, graph_connection_set const &connections) {
+}
--- audio/audio_engine/rendering/yas_audio_rendering_graph.h
+++ audio/audio_engine/rendering/yas_audio_rendering_graph.h
@@ -4,11 +4,15 @@
 
 #pragma once
 
+#include <audio/yas_audio_graph_connection.h>
+#include <audio/yas_audio_graph_node.h>
 #include <audio/yas_audio_rendering_node.h>
 
 namespace yas::audio {
 struct rendering_graph {
     std::vector<rendering_node> const nodes;
+
+    rendering_graph(graph_node_set const &, graph_connection_set const &);
 
    private:
     rendering_graph(rendering_graph const &) = delete;
