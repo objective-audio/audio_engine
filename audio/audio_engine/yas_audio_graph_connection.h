685c9046795b781d6af5c29b886fa352a334c941
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-20 00:42:05 +0900

    renderable_graph_connection

--- audio/audio_engine/yas_audio_graph_connection.h
+++ audio/audio_engine/yas_audio_graph_connection.h
@@ -12,11 +12,11 @@ namespace yas::audio {
 struct graph_connection : graph_node_removable, renderable_graph_connection {
     virtual ~graph_connection();
 
-    uint32_t source_bus() const;
+    uint32_t source_bus() const override;
     uint32_t destination_bus() const;
-    audio::graph_node_ptr source_node() const;
+    audio::graph_node_ptr source_node() const override;
     audio::graph_node_ptr destination_node() const;
-    audio::format const &format() const;
+    audio::format const &format() const override;
 
    private:
     uint32_t const _source_bus;
