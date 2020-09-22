dff0c22abd501bf54db95eae1210b6abf3082e2e
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-22 12:29:36 +0900

    connection source_node

--- audio/audio_engine/rendering/yas_audio_rendering_connection.h
+++ audio/audio_engine/rendering/yas_audio_rendering_connection.h
@@ -14,12 +14,10 @@ class rendering_node;
 struct rendering_connection {
     uint32_t const source_bus_idx;
     audio::format const format;
+    rendering_node const *const source_node;
 
     rendering_connection(uint32_t const src_bus_idx, rendering_node const *const src_node, audio::format const format);
 
     bool render(audio::pcm_buffer *const, audio::time const &) const;
-
-   private:
-    rendering_node const *const source_node;
 };
 }  // namespace yas::audio
