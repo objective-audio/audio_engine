f65d24f7c9e28ce9da15130c8251ab2485a478e2
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-27 14:46:53 +0900

    device tests

--- audio_tests/audio_device_tests/yas_audio_device_tests.mm
+++ audio_tests/audio_device_tests/yas_audio_device_tests.mm
@@ -7,9 +7,11 @@
 #if (TARGET_OS_MAC && !TARGET_OS_IPHONE)
 
 #import <audio/yas_audio_mac_device.h>
+#import "yas_audio_test_io_device.h"
 #import "yas_audio_test_utils.h"
 
 using namespace yas;
+using namespace yas::test;
 
 @interface yas_audio_device_tests : XCTestCase
 
@@ -23,6 +25,22 @@ using namespace yas;
 
 - (void)tearDown {
     [super tearDown];
+}
+
+- (void)test_empty_channel_count {
+    auto const device = test_io_device::make_shared();
+
+    XCTAssertEqual(device->input_channel_count(), 0);
+    XCTAssertEqual(device->output_channel_count(), 0);
+}
+
+- (void)test_channel_count {
+    auto const device = test_io_device::make_shared();
+    device->input_format_handler = []() { return audio::format{{.sample_rate = 44100.0, .channel_count = 1}}; };
+    device->output_format_handler = []() { return audio::format{{.sample_rate = 44100.0, .channel_count = 2}}; };
+
+    XCTAssertEqual(device->input_channel_count(), 1);
+    XCTAssertEqual(device->output_channel_count(), 2);
 }
 
 @end
