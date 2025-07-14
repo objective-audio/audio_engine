7322efa076253458f4a31cc2db3019c1851cbba3
Yuki Yasoshima <yuki@MacBook-Air.local>
2025-07-14 21:17:52 +0900

    update package version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -78,6 +78,6 @@ let package = Package(
             ]
         ),
     ],
-    cLanguageStandard: .gnu18,
+    cLanguageStandard: .gnu2x,
     cxxLanguageStandard: .gnucxx2b
 )
